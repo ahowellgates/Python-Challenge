046ed2466277580082dbba72acf60c899f80a970
Ashley Gates <ashleygates@Ashleys-MBP.attlocal.net>
2021-07-05 18:50:31 -0400

    new new

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,6 +5,27 @@ import csv
 # Path to collect data from the Resources folder
 pypoll_csv = os.path.join('PyPoll','Resources','election_data.csv')
 
+with open(pypoll_csv, 'r') as csvfile:
+
+    # Split the data on commas
+    csvreader = csv.DictReader(csvfile, delimiter=',')
+
+    #rows = list(csvreader)
+    #totalrows = len(rows)
+
+votes = []
+
+for row in csvreader:
+     candidate_name = row['Candidate']
+     votes.append(candidate_name)
+
+print(len(votes))
+
+
+
+
+
+
 
 
 
