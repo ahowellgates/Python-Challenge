38e1575bd94250c7a4523ddbb34d4fd9c4ebfa6d
Ashley Gates <ashleygates@Ashleys-MBP.attlocal.net>
2021-07-05 18:24:48 -0400

    new new

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,7 +3,8 @@ import os
 import csv
 
 # Path to collect data from the Resources folder
-pybank_csv = os.path.join('..','Resources','election_data.csv')
+pypoll_csv = os.path.join('PyPoll','Resources','election_data.csv')
+
 
 
 
@@ -16,3 +17,6 @@ pybank_csv = os.path.join('..','Resources','election_data.csv')
 
 
 #Print to both the terminal and a txt file
+
+#Print ( ---, file=open("output.txt", "a"))--six times for each print statement
+
