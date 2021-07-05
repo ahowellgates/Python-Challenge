5ce862978b835c4b54f336a3b6b13cce58d5c8e1
Ashley Gates <ashleygates@Ashleys-MBP.attlocal.net>
2021-07-05 15:47:20 -0400

    new code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,30 @@
+#import dependecies
 import os
 import csv
+import datetime
 
-pybank_csv = os.path.join('..','Resources','budget_data.csv')
+# Path to collect data from the Resources folder
+pybank_csv = os.path.join('Resources','budget_data.csv')
+
+with open(pybank_csv, 'r') as csvfile:
+
+    # Split the data on commas
+    csvreader = csv.DictReader(csvfile, delimiter=',')
+    
+    #rows = list(csvreader)
+    #totalrows = len(rows)
+    months = 0
+    net_profit = 0
+    current_profit = 0
+    last_profit = 0
+    changes_profit = 0
+    greatest_increase = 0
+    greatest_decrease = 0
+
+    for row in csvreader:
+        months += 1
+
+            Print(months)
 
 
 
@@ -9,3 +32,13 @@ pybank_csv = os.path.join('..','Resources','budget_data.csv')
 
 
 
+
+
+
+
+
+
+
+
+
+#Print to both the terminal and a txt file
---
+++ PyPoll/main.py
@@ -0,0 +1,18 @@
+#import dependencies
+import os
+import csv
+
+# Path to collect data from the Resources folder
+pybank_csv = os.path.join('..','Resources','election_data.csv')
+
+
+
+
+
+
+
+
+
+
+
+#Print to both the terminal and a txt file
